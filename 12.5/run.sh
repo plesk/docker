bd95f082ca4ebf67ec80758a54d3981ac02f5c6c
Michael Zarubin <mzarubin@odin.com>
2015-09-02 20:55:52 +0600

    * Optimize operations with plesk installer * Switch to last release in 12.5.x branch * Use cloning utility instead of db operations * Add cleanup step, to reduce image size * Add hints about known ports to expose in Dockerfile
    Signed-off-by: Michael Zarubin <mzarubin@odin.com>

--- 12.5/run.sh
+++ 12.5/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-/etc/init.d/psa startall
-plesk bin ipmanage --reread
-/etc/init.d/nginx start
+export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:$PATH
+psa_service execute_actions
+psa_service startall
+plesk sbin pleskrc nginx start
 exec plesk log --all
