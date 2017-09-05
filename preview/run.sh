382cbfc8062f37db89b32bbcd7cd3098cef821a6
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-09-05 21:59:51 +0700

    Option prepare-public-image prohibits to set admin password before cloning.

--- preview/run.sh
+++ preview/run.sh
@@ -3,6 +3,10 @@
 export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:$PATH
 service mysql start
 psa_service execute_actions
+if [ -f /root/set_default_password ]; then
+    plesk bin admin --set-password -passwd changeme
+    rm /root/set_default_password
+fi
 psa_service startall
 plesk sbin pleskrc nginx start
 cron
