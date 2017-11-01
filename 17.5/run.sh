4db915417f2023237905029c6fc9153650ecf865
Alexei Yuzhakov <ayuzhakov@plesk.com>
2017-11-01 21:28:16 +0700

    Backport password helper to stable branch.

--- 17.5/run.sh
+++ 17.5/run.sh
@@ -3,6 +3,12 @@
 export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:$PATH
 service mysql start
 psa_service execute_actions
+if [ -f /root/set_admin_password ]; then
+    ADMIN_PASSWORD="changeme"
+    [ -n "$PSA_PASSWORD" ] && ADMIN_PASSWORD=""
+    plesk bin admin --set-password -passwd "$ADMIN_PASSWORD"
+    rm /root/set_admin_password
+fi
 psa_service startall
 plesk sbin pleskrc nginx start
 cron
