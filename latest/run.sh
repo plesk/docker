fbc46f0112e7b44341b9fd0c92f4f3038c352ffd
Alexei Yuzhakov <ayuzhakov@plesk.com>
2020-02-12 23:31:00 +0700

    Sync strong password in post-install

--- latest/run.sh
+++ latest/run.sh
@@ -4,7 +4,7 @@ export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:$PATH
 find /var/lib/mysql -type f -exec touch {} \; && service mysql start
 psa_service execute_actions
 if [ -f /root/set_admin_password ]; then
-    ADMIN_PASSWORD="changeme"
+    ADMIN_PASSWORD="changeme1Q**"
     [ -n "$PSA_PASSWORD" ] && ADMIN_PASSWORD=""
     plesk bin admin --set-password -passwd "$ADMIN_PASSWORD"
     rm /root/set_admin_password
