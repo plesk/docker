568f92dbfa22610db1c426c43db21bd88222627e
Alexei Yuzhakov <ayuzhakov@plesk.com>
2020-09-24 12:08:17 +0700

    Basic setup for remote MySQL support

--- latest/run.sh
+++ latest/run.sh
@@ -12,8 +12,11 @@ done <<<  `grep --no-filename -rE '^d\s' /usr/lib/tmpfiles.d/*.conf`
 # create plesk-web-socket directory manually due fake systemctl can't do it
 install -m 770 -o psaadm -g psaadm -d /run/plesk-web-socket
 
-# mysql should be started by sysvinit script due fake systemctl can't handle `unset-environment`
-find /var/lib/mysql -type f -exec touch {} \; && service mysql start
+if [ "x$MYSQL_HOST" = "x" ]; then
+	# mysql should be started by sysvinit script due fake systemctl can't handle `unset-environment`
+	chmod +x /usr/sbin/mysqld
+	find /var/lib/mysql -type f -exec touch {} \; && service mysql start
+fi
 
 # remap IP
 plesk bin reconfigurator --autoconfigure
@@ -24,11 +27,21 @@ if [ -f /root/first_run ]; then
 	/opt/psa/admin/sbin/reset_instance_data start --do-what-I-say
 
 	# admin password
-    ADMIN_PASSWORD="changeme1Q**"
+	ADMIN_PASSWORD="changeme1Q**"
 	[ -n "$PSA_PASSWORD" ] && ADMIN_PASSWORD=""
 	plesk bin admin --set-password -passwd "$ADMIN_PASSWORD"
 
 	rm /root/first_run
+fi
+
+if [ "x$MYSQL_HOST" != "x" ]; then
+	mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD psa < /root/psa.sql
+	mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD < /root/apsc.sql
+	mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON apsc.* TO 'admin'@'%';"
+	echo -e "[general]\ndatabase.dbname=psa\ndatabase.username=admin\ndatabase.host=$MYSQL_HOST\ndatabase.password=$MYSQL_PASSWORD" > /etc/psa/private/dsn.ini
+	plesk bin settings -s aps_login=admin
+	plesk bin settings -s aps_host=$MYSQL_HOST
+	plesk bin settings -s aps_password=$MYSQL_PASSWORD
 fi
 
 # startup services required by Plesk
